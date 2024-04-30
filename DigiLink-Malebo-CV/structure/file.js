2168ba78ca61af3b0d87950f5c949747aee08091
Malebo <114099916+molelekimc@users.noreply.github.com>
2024-04-30 14:21:32 +0200

    Add files via upload

---
+++ DigiLink-Malebo-CV/structure/file.js
@@ -0,0 +1,14 @@
+// create function
+function downloadPDF(){
+    console.log("Working on it");
+    // Print the current page
+    window.print();
+    alert("Downloaded Successfully");
+} 
+
+
+
+
+
+
+
